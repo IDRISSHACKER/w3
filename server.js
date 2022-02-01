f2809da20bc71dbc4aea8be7466a6877c925e636
IDRISSHACKER <idrisscoder@gmail.com>
2022-02-01 01:44:43 +0100

    instantiation

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import Express from "express";
+require("dotenv").config()
+const Express = require("express");
 
 const APP_NAME      = "W3"
 const RESPONSE_TYPE = "text/html"
-const HELLO_MESSAGE = `<h1>Bienvenue sur ${APP_NAME}</h1>`
+const HELLO_MESSAGE = `<h1>Bienvenue sur ${APP_NAME} ==> ${process.env.AUTHOR} </h1>`
 const SERVER_PORT   = 3000
 const STATUS_ERROR  = 404
 const STATUS_SUCCESS= 200
@@ -11,7 +12,7 @@ var app = Express()
 
 app.get("/", (_request, response)=>{
     response.setHeader("Content-Type", RESPONSE_TYPE)
-    response.status(STATUS_SUCCESS)
+    response.status(200)
     response.send(HELLO_MESSAGE)
 })
 
